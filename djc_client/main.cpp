4dbbcf0b81a062c18378b2a3bae79c56f76cc344
li <1569346500@qq.com>
2024-03-06 16:15:41 +0800

    second

--- djc_client/main.cpp
+++ djc_client/main.cpp
@@ -4,7 +4,7 @@ int main()
   CTcpClient TcpClient;
  
   // 向服务器发起连接请求
-  if (TcpClient.ConnectToServer("192.168.26.129",5001)==false)
+  if (TcpClient.ConnectToServer("192.168.26.131",5001)==false)
   { printf("TcpClient.ConnectToServer(\"172.16.0.15\",5051) failed,exit...\n"); return -1; }
  
   char strbuffer[1024];
@@ -12,7 +12,7 @@ int main()
   for (int ii=0;ii<50;ii++)
   {
     memset(strbuffer,0,sizeof(strbuffer));
-    sprintf(strbuffer,"这是第%d个超级女生，编号%03d。",ii+1,ii+1);
+    sprintf(strbuffer,"这是第%d个学生，编号%03d。",ii+1,ii+1);
     if (TcpClient.Send(strbuffer,strlen(strbuffer))<=0) break;
     printf("发送：%s\n",strbuffer);
    
